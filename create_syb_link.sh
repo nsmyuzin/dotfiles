332122d358673165393da422629c891103eebf25
myuzin <nsmyuzin@gmail.com>
2017-07-27 16:22:56 +0900

    create_syn_link_shell_file

---
+++ create_syb_link.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/indent ~/.vim
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zsh.d/alias.zsh ~/alias.zsh
+ln -sf ~/dotfiles/.zsh.d/function.zsh ~/function.zsh
